c8457ba1e367dd1da7c955c293eb0599fd1faeb6
Seung-Tak Noh <seungtak.noh@gmail.com>
2025-08-14 00:14:15 +0900

    # major update MiniGLSLSandbox.cpp
    - default vertex shader: "surfacePosAttrib" to update "surfacePosition"
    - vertex attribute L/R/B/T for fragment shader
    - add "surfaceSize" for fragment shader input
    - mouse interaction for zoom/pan

--- apps/app105/MiniGLSLSandbox.cpp
+++ apps/app105/MiniGLSLSandbox.cpp
@@ -11,13 +11,16 @@
 
 std::string shader_vs = R"(
 #version 330 core
-in vec2 vert;
+layout(location=0) in vec2 vert;
+layout(location=1) in vec2 surfacePosAttrib;
+out vec2 surfacePosition;
 
 uniform mat4 mvp;
 
 void main()
 {
-	gl_Position = mvp * vec4(vert, 1.0f, 1.0f);
+	surfacePosition = surfacePosAttrib;
+	gl_Position = mvp * vec4(vert, 0.0f, 1.0f);
 }
 )";
 std::string shader_fs = R"(
@@ -42,6 +45,9 @@ public:
 	void Draw(const int width, const int height)
 	{
 		const float size = 1.0f;
+		float aspect = (float)width / (float)height;
+
+		GLuint loc = -1;
 
 		if (use_shader) {
 			shader->Enable();
@@ -59,7 +65,6 @@ public:
 			float y = 1.0f - mouse_this_y / (float)height;
 
 			// pass data to shader
-			GLuint loc;
 			loc = shader->GetUniformLocation("mvp");
 			if (-1 != loc) glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(identity));
 
@@ -75,21 +80,34 @@ public:
 			if (-1 != loc) glUniform2f(loc, (float)width, (float)height);
 			//printf("resolution: %4d %4d\r", width, height);
 
-			// [TODO] "surfaceSize"
+			loc = shader->GetUniformLocation("surfaceSize");
+			if (-1 != loc) glUniform2f(loc, aspect, 1.0f);
+			//printf("surfaceSize: %7.3f %7.3f\r", width, 1.0f);
 
-			// [TODO] "backbuffer"
+			// [CAUTION] last query should be "backbuffer"
+			//loc = shader->GetUniformLocation("backbuffer");
 		}
 
-		// bluish color to show non-shaded image
-		glBegin(GL_TRIANGLES);
-		glColor3f(0.0, 0.0f, 0.5f); glVertex2f(-size, -size); // left bottom
-		glColor3f(1.0, 0.0f, 0.5f); glVertex2f(+size, -size); // right bottom
-		glColor3f(1.0, 1.0f, 0.5f); glVertex2f(+size, +size); // right top
+		// compute 
+		float L = center_x - 0.5f * fov * aspect;
+		float R = center_x + 0.5f * fov * aspect;
+		float B = center_y - 0.5f * fov;
+		float T = center_y + 0.5f * fov;
 
-		glColor3f(0.0, 0.0f, 0.5f); glVertex2f(-size, -size); // left bottom
-		glColor3f(1.0, 1.0f, 0.5f); glVertex2f(+size, +size); // right top
-		glColor3f(0.0, 1.0f, 0.5f); glVertex2f(-size, +size); // left top
-		glEnd();
+		// bluish color to show non-shaded image
+		auto DrawQuad = [&] {
+			glBegin(GL_TRIANGLES);
+			glColor3f(0.0, 0.0f, 0.5f); glVertexAttrib2f(1, L, B); glVertex2f(-size, -size); // left bottom
+			glColor3f(1.0, 0.0f, 0.5f); glVertexAttrib2f(1, R, B); glVertex2f(+size, -size); // right bottom
+			glColor3f(1.0, 1.0f, 0.5f); glVertexAttrib2f(1, R, T); glVertex2f(+size, +size); // right top
+
+			glColor3f(0.0, 0.0f, 0.5f); glVertexAttrib2f(1, L, B); glVertex2f(-size, -size); // left bottom
+			glColor3f(1.0, 1.0f, 0.5f); glVertexAttrib2f(1, R, T); glVertex2f(+size, +size); // right top
+			glColor3f(0.0, 1.0f, 0.5f); glVertexAttrib2f(1, L, T); glVertex2f(-size, +size); // left top
+			glEnd();
+		};
+
+		DrawQuad();
 
 		if (use_shader) shader->Disable();
 	}
@@ -104,13 +122,18 @@ public:
 		shader->Compile(shader_fs, vgl::SHADER_TYPE::FRAGMENT);
 		shader->Link();
 
+		InitUserInteraction();
+
 		time_stamp = std::chrono::system_clock::now();
 
-		TwDefine("Bar size='100 50'");
+		TwDefine("Bar size='150 250'");
 		TwAddVarRW(bar, "UseShader", TwType::TW_TYPE_BOOLCPP, &use_shader, "key=SPACE");
-		TwAddButton(bar, "Reload", [](void* client) {
-			((MiniGLSLSandbox*)client)->ReloadShader();
+		TwAddButton(bar, "LoadShader", [](void* client) {
+			((MiniGLSLSandbox*)client)->LoadShader();
 		}, this, "key=L");
+		TwAddVarRW(bar, "fov", TwType::TW_TYPE_FLOAT, &fov, "min=0.001 step=0.001");
+		TwAddVarRW(bar, "center_x", TwType::TW_TYPE_FLOAT, &center_x, "step=0.001");
+		TwAddVarRW(bar, "center_y", TwType::TW_TYPE_FLOAT, &center_y, "step=0.001");
 
 		return true;
 	}
@@ -122,8 +145,31 @@ public:
 		}
 	}
 
+	bool Update()
+	{
+		float aspect = (float)width / (float)height;
+
+		int diff_x = mouse_this_x - mouse_prev_x;
+		int diff_y = mouse_this_y - mouse_prev_y;
+
+		// left drag
+		if (mouse_button[0]) {
+			center_x -= fov * (float)diff_x / (float)height;
+			center_y += fov * (float)diff_y / (float)width * aspect;
+		}
+
+		// middle drag
+		if (mouse_button[2]) {
+			fov *= pow(0.997, diff_x + diff_y);
+		}
+
+		mouse_prev_x = mouse_this_x;
+		mouse_prev_y = mouse_this_y;
+		return true;
+	}
+
 private:
-	void ReloadShader()
+	void LoadShader()
 	{
 		char const* filterPatterns[1] = { "*.frag" };
 
@@ -155,11 +201,32 @@ private:
 			}
 			else {
 				time_stamp = std::chrono::system_clock::now();
+				InitUserInteraction();
 			}
 		}
 	}
 
 	bool use_shader = true;
+
+
+	////////////////////////////////////////
+	// user interaction
+	////////////////////////////////////////
+	void InitUserInteraction()
+	{
+		// initialize user interaction data
+		center_x = 0.0f;
+		center_y = 0.0f;
+		fov = 1.0f;
+	}
+	float center_x;
+	float center_y;
+	float fov;
+
+	// reserve previous mouse position
+	int mouse_prev_x;
+	int mouse_prev_y;
+
 	std::chrono::system_clock::time_point time_stamp;
 
 	vgl::GLShader* shader = nullptr;
