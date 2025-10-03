ab0e7d9235d9c7ac27af86a2454467b608327f15
Seung-Tak Noh <seungtak.noh@gmail.com>
2025-10-03 10:41:27 +0900

    # some fixes for lecture-04
    - add z_near/z_far to check z-fighting issue
    - add a single object drawing ("DRAW2")
    - finer grid for vgl::drawGridXZ

--- apps/app104/DepthTestAppGL.cpp
+++ apps/app104/DepthTestAppGL.cpp
@@ -15,8 +15,8 @@ public:
 		glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glm::mat4 proj = glm::infinitePerspective(glm::radians(53.1301f), width / (float)height, 0.1f);
-		glm::mat4  view = glm::translate(glm::mat4(1.0f), -GlobalViewPosition);
+		glm::mat4 proj = glm::perspective(glm::radians(53.1301f), width / (float)height, z_near, z_far);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), -GlobalViewPosition);
 		view = view * glm::mat4_cast(GlobalViewRotation);
 
 		glMatrixMode(GL_PROJECTION); glLoadMatrixf(glm::value_ptr(proj));
@@ -25,7 +25,7 @@ public:
 		// draw XZ floor plane
 		glColor3f(0.0f, 0.0f, 0.0f);
 		glLineWidth(1.0f);
-		vgl::drawGridXZ(20.0f, 20);
+		vgl::drawGridXZ(20.0f, 50);
 
 		// set 
 		glDisable(GL_CULL_FACE);
@@ -34,7 +34,7 @@ public:
 		if (cull_face ) glEnable(GL_CULL_FACE);
 		if (depth_test) glEnable(GL_DEPTH_TEST);
 
-		glLineWidth(2.0f);
+		glLineWidth(3.0f);
 
 		// draw cube
 		auto drawCubeWithOffset = [](glm::vec3 tr) {
@@ -54,12 +54,13 @@ public:
 		};
 
 		// offsets for cube drawing
-		glm::vec3 offset1 = glm::vec3(0.0f, 0.5f, 0.0f);
-		glm::vec3 offset2 = glm::vec3(-1.2f, 0.5f, +1.2f);
+		glm::vec3 offset1 = glm::vec3(-1.2f, 0.5f, +1.2f);
+		glm::vec3 offset2 = glm::vec3( 0.0f, 0.5f,  0.0f);
 		glm::vec3 offset3 = glm::vec3(+1.2f, 0.5f, -1.2f);
 
 		std::vector<glm::vec3> offsets;
 
+		// ordering draws
 		switch (draw_order)
 		{
 		case 0: offsets = { offset1, offset2, offset3 }; break;
@@ -68,7 +69,10 @@ public:
 		case 3: offsets = { offset2, offset3, offset1 }; break;
 		case 4: offsets = { offset3, offset1, offset2 }; break;
 		case 5: offsets = { offset3, offset2, offset1 }; break;
-		default: break;
+		case -1: 
+		default: 
+			offsets = { offset2 }; // draw single object
+			break;
 		}
 
 		for (auto it : offsets)
@@ -99,11 +103,12 @@ public:
 		TwAddVarRW(bar, "Global-posY", TwType::TW_TYPE_FLOAT, &GlobalViewPosition.y, "group='Global' label='posY' step=0.01");
 		TwAddVarRW(bar, "Global-posZ", TwType::TW_TYPE_FLOAT, &GlobalViewPosition.z, "group='Global' label='posZ' step=0.01");
 #endif
-		// 1) back face culling
+		// 1) toggle backface culling
 		TwAddVarRW(bar, "cull_face" , TwType::TW_TYPE_BOOLCPP, &cull_face , "label='cull_face' ");
 
 		// 2) painter's algorithm
 		TwEnumVal drawOrderEV[] = {
+			{-1, "DRAW2" },
 			{0, "DRAW123"},
 			{1, "DRAW132"},
 			{2, "DRAW213"},
@@ -112,11 +117,15 @@ public:
 			{5, "DRAW321"}
 		};
 
-		TwType drawOrderType = TwDefineEnum("DrawOrder", drawOrderEV, 6);
+		TwType drawOrderType = TwDefineEnum("DrawOrder", drawOrderEV, 7);
 		TwAddVarRW(bar, "draw_order", drawOrderType, &draw_order, NULL);
 
-		// 3) z-buffer
+		// 3) toggle z-buffer
 		TwAddVarRW(bar, "depth_test", TwType::TW_TYPE_BOOLCPP, &depth_test, "label='depth_test' ");
+
+		// 4) clipping plane values to check z-fighting issue
+		TwAddVarRW(bar, "z_near", TwType::TW_TYPE_FLOAT, &z_near, "label='z_near' ");
+		TwAddVarRW(bar, "z_far" , TwType::TW_TYPE_FLOAT, &z_far , "label='z_far'  ");
 
 		return true;
 	}
@@ -127,9 +136,12 @@ public:
 
 private:
 
-	int draw_order = 0;
+	int  draw_order = -1; // initial value
 	bool cull_face = false;
 	bool depth_test = false;
+
+	float z_near =   0.1f;
+	float z_far  = 100.0f;
 
 	// global viewer
 	glm::quat GlobalViewRotation;
